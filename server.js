e42f21bcec5c2a88921fb3834a11a8216937f669
Diego Abner <diego@diegoabner.com>
2026-07-20 23:44:22 -0300

    feat: add premium dark oferta11 based on design system

--- server.js
+++ server.js
@@ -129,6 +129,10 @@ app.get('/oferta10', (req, res) => {
   res.sendFile(path.join(__dirname, 'oferta10.html'));
 });
 
+app.get('/oferta11', (req, res) => {
+  res.sendFile(path.join(__dirname, 'oferta11.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
